df48b721ae16f3e39fb06dd7d0b7123dadfd940c
zhl <zhuhaoliang2016@163.com>
2024-04-02 17:59:40 +0800

    增加hot100

---
+++ host100/pu_tong_shu_zu_test.go
@@ -0,0 +1,84 @@
+package host100
+
+import (
+	"math"
+	"testing"
+)
+
+// leetcode 53 最大子数组和
+
+/*
+给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+子数组
+是数组中的一个连续部分
+
+
+示例 1：
+
+输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+输出：6
+解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+示例 2：
+
+输入：nums = [1]
+输出：1
+示例 3：
+
+输入：nums = [5,4,-1,7,8]
+输出：23
+
+如果维护了一个最大的前缀值得话，当出现负值的时候，就会出现 [1. -1] 的情况，导致计算结果出错。
+	nums = []int{-1}
+	// 测试结果
+	result = maxSubArray(nums)
+	t.Log(result)
+https://leetcode.cn/problems/maximum-subarray/solutions/2533977/qian-zhui-he-zuo-fa-ben-zhi-shi-mai-mai-abu71/
+*/
+
+func maxSubArray(nums []int) int {
+	prefixSum := 0
+	minPrefixSum := 0
+	// maxprefixSum := 0
+	result := math.MinInt
+	for _, num := range nums {
+		prefixSum += num
+		// 计算最小的前缀和。
+
+		// if prefixSum > maxprefixSum {
+		// 	maxprefixSum = prefixSum
+		// }
+		// 取结果，和最大前缀-最小前缀的值得最大的一个。
+		if prefixSum-minPrefixSum > result {
+			result = prefixSum - minPrefixSum
+		}
+		// else {
+		// 	result = prefixSum
+		// }
+		if prefixSum < minPrefixSum {
+			minPrefixSum = prefixSum
+		}
+
+	}
+	return result
+}
+
+// test
+func TestPuTongShuZu(t *testing.T) {
+	// 测试用例
+	nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	// 测试结果
+	result := maxSubArray(nums)
+	t.Log(result)
+
+	nums = []int{5, 4, -1, 7, 8}
+	// 测试结果
+	result = maxSubArray(nums)
+	t.Log(result)
+
+	nums = []int{-1}
+	// 测试结果
+	result = maxSubArray(nums)
+	t.Log(result)
+
+}

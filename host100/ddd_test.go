ccac3f895bacef6add92093f99f87e42af638082
XTZJ-20210622GK\Administrator <zhuhaoliang2016@163.com>
2024-05-23 21:45:07 +0800

    增加netsta的文件

--- host100/ddd_test.go
+++ host100/ddd_test.go
@@ -1,6 +1,9 @@
 package host100
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func quickSort2(arr []int) {
 	if len(arr) <= 1 {
@@ -23,13 +26,14 @@ func partition2(arr []int, left, right int) int {
 	i := left
 	for j := left; j < right; j++ {
 		// 小于基准值的放在左边边，大于基准值的放在右边。当大于基准值的时候，这个值得位置不变，并且i也不变，
-		// 所以最后循环完后，i就是新的基准值的位置。
-		if arr[j] > tmp {
+		// 所以最后循环完后，i就是新的基准值的位置。j会比i走的更快一点的.所以最后就是小于基准值的都跑到左边了.
+		if arr[j] < tmp {
 			arr[i], arr[j] = arr[j], arr[i]
 			i++
 		}
 	}
 	arr[i], arr[right] = arr[right], arr[i]
+	fmt.Println(arr)
 	return i
 }
 
@@ -39,4 +43,33 @@ func TestQuickSort2(t *testing.T) {
 	for _, item := range arr {
 		t.Log(item)
 	}
+}
+
+func quickSort3(num []int) {
+	if len(num) < 2 {
+		return
+	}
+
+}
+
+func QuickSort3(num []int, left, right int) {
+	if left > right {
+		return
+	}
+	index := partition3(num, left, right)
+	QuickSort3(num, index+1, right)
+	QuickSort3(num, left, index-1)
+}
+
+func partition3(num []int, left, right int) int {
+	mid := num[right]
+	i := left
+	for j := left; j < right; j++ {
+		if num[j] < mid {
+			num[i], num[j] = num[j], num[i]
+			i++
+		}
+	}
+	num[i], num[right] = num[right], num[i]
+	return i
 }
